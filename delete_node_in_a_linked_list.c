63baadc265e667991feb3ddce7b21d62b76aec83
zjc-enigma <enigma@pku.edu.cn>
2015-10-28 23:50:15 +0800

    late

---
+++ delete_node_in_a_linked_list.c
@@ -0,0 +1,36 @@
+/** * Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* }; */
+
+/* Write a function to delete a node (except the tail) in a singly linked list,
+given only access to that node.Supposed the linked list is
+1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+the linked list should become 1 -> 2 -> 4 after calling your function. */
+
+
+struct ListNode {
+     int val;
+     struct ListNode *next;
+};
+
+void deleteNode(struct ListNode *node) {
+
+  while(node->next->next != NULL) {
+    node->val = node->next->val;
+    node = node->next;
+  }
+  // tail node
+  node->val = node->next->val;
+  free(node->next);
+  node->next = NULL;
+
+}
+
+
+void test(){
+
+
+
+}
